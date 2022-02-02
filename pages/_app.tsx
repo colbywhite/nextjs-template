8c7bc724575006a7e1cf0f04bea6bf77eb9d60ed
Colby M. White <colbywhite@users.noreply.github.com>
2022-02-01 23:38:24 -0600

    feat: add normalize, sakura, material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,9 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import 'normalize.css'
+import 'sakura.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
