ce44ba6e9e26c4102aca19e6cd11ec95e84e93e3
Colby M. White <colbywhite@users.noreply.github.com>
2022-08-02 23:01:35 -0500

    feat: add tailwind; use mui reset; better copy

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ with what I consider a sane set of starting settings.
 
 It contains the following:
 - Basic Hello, World page
-- Sane npm scripts
-- `normalize.css` and `sakura.css` setup
-- `material-ui`
-- Simple API usage example
+- `material-ui` components
+- A two-color theme
+- CSS reset via `CssBaseline`
+- `tailwindcss`
 
 [Colby M. White]: https://github.com/colbywhite/
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,42 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import type {NextPage} from 'next';
-import {useEffect, useState} from 'react';
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+import Box from '@mui/material/Box';
+import Image from 'next/image';
+import logo from '../public/nextjs.svg';
+
+function Header() {
+  return (
+    <Box component="header" className="flex flex-row justify-center">
+      <Image src={logo} className="h-28 pointer-events-none" alt="logo"/>
+    </Box>
+  );
+}
+
 
 const Home: NextPage = () => {
-  const [name, setName] = useState('World');
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(res => res.json())
-      .then(({name}) => setName(name));
-  }, []);
   return (
-    <>
+    <Container className="max-w-xl">
       <Header/>
-      <main>
-        <Typography variant="h1" gutterBottom>Lorem Ipsum</Typography>
-
-        <Card component="section">
-          <CardContent>
-            <Typography variant="h4" color="text.secondary" gutterBottom>{name}</Typography>
-            <Typography variant="body1">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non malesuada nisl.
-            </Typography>
-          </CardContent>
-        </Card>
-      </main>
-      <Footer/>
-    </>
+      <Container component="main" className="flex flex-col gap-5 justify-between">
+        <Typography variant="body1">
+          This is a starting template for a <code>create-next-app</code> project with what I consider a sane set of
+          starting settings.
+        </Typography>
+        <ul className="list-disc">
+          <li>The basic &quot;Hello, World&quot; page you&apos;re looking at now</li>
+          <li><code>material-ui</code> components</li>
+          <li>A two-color theme:&nbsp;
+            <Chip color="primary" label="primary"></Chip>
+            &nbsp;
+            <Chip color="secondary" label="secondary"></Chip>
+          </li>
+          <li>CSS reset via <code>CssBaseline</code></li>
+          <li><code>tailwindcss</code></li>
+        </ul>
+      </Container>
+    </Container>
   );
 };
 
